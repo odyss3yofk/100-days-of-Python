3250caccd257051e3acf40d376d2dab33cfefad8
Kuldeep Bora <kuldeepbora.k@gmail.com>
2024-12-08 14:14:09 +0530

    first step

--- day 1/bandgenerator.py
+++ day 1/bandgenerator.py
@@ -1,2 +1,7 @@
-print("Enter Your Hometown Name")
-input
+print("Welcome to Band Generator")
+
+city_name= input("What is the name of the city you grew up in ? \n")
+
+pet_name=input("What is your pet name ? \n")
+
+print("Your band name could be "+city_name+" "+pet_name+"!")
